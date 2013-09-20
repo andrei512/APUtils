1b2dbab0c7380462c858e8288696e47b06de9f5b
Andrei Puni <andrei.puni@yardi.com>
2013-09-20 12:17:40 +0300

    stoped tracking Pods

--- DevAPUtils/Pods/BuildHeaders/MAObjCRuntime/RTIvar.h
+++
@@ -1,1 +0,0 @@
-../../MAObjCRuntime/RTIvar.h
--- DevAPUtils/Pods/Pods-MAObjCRuntime-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MAObjCRuntime : NSObject
-@end
-@implementation PodsDummy_Pods_MAObjCRuntime
-@end
