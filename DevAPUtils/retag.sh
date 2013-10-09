f291aa5c89a0f8708d261af29d23ac0c2cda819c
andrei512 <puni.andrei23@gmail.com>
2013-10-09 16:02:18 +0300

    testing podspec

--- DevAPUtils/retag.sh
+++ DevAPUtils/retag.sh
@@ -5,9 +5,10 @@ git add . --all
 git ci -am "$2"
 # remove old tag
 git tag -d $1
-# create new one
-git tag $1 -m "$1"
+git push --tags 
 # push changes 
 git push 
+# create new one
+git tag $1 -m "$1"
 # push tags
 git push --tags 
